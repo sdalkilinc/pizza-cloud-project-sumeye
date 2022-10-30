b11dae4771a915f6df499876ec2cba4606fddabe
Sumeye <dalkilincsumeye@gmail.com>
2022-10-29 23:31:49 -0400

    More work added

--- src/main/java/com/cydeo/controller/DesignPizzaController.java
+++ src/main/java/com/cydeo/controller/DesignPizzaController.java
@@ -5,6 +5,7 @@ import com.cydeo.model.Pizza;
 import com.cydeo.repository.PizzaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,8 +17,10 @@ public class DesignPizzaController {
 
     private PizzaRepository pizzaRepository;
 
-    @PostMapping
+    @GetMapping
     public String showDesignForm(Model model) {
+
+        model.addAttribute("pizza", new Pizza());
 
         model.addAttribute("cheeses", DataGenerator.cheeseTypeList);
         model.addAttribute("sauces", DataGenerator.sauceTypeList);
